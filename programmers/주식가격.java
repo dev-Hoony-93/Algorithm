52966cfbf54115ec18efe4924d5317edba9f8dba
hoony93 <hoon7566@gmail.com>
2021-08-30 12:45:18 +0900

    프로그래머스 주식가격

--- programmers/주식가격.java
+++ programmers/주식가격.java
@@ -1,21 +1,36 @@
-/*******************************************************************************
- *  COPYRIGHT(C) KYOBO BOOK CENTRE ALL RIGHTS RESERVED.
- * This software is the proprietary information of Kyobo Book.
- *
- * Revision History
- * Author   Date            Description
- * ------   ----------      ------------------------------------
- * 김윤환   	2021. 8. 30.         First Draft.
- *
- *******************************************************************************/
 package programmers;
 
+import java.util.Stack;
+
 /**
- *  
- * 개발자 : <a href="mailto:yhk1712@kico.co.kr">김윤환</a>
- * 생성일자 : 2021. 8. 30.
- * 
- */
+ *
+ * https://programmers.co.kr/learn/courses/30/lessons/42584
+ *
+ * 프로그래머스 - 주식가격
+ *
+ * - 처음에는 2중반복문을 통한 브루트포스로 풀었으나,
+ * - 스택을 ㅇ용한 풀이가 있는것을 알고 다시 풀이.
+ *
+ * */
 public class 주식가격 {
+
+	public int[] solution(int[] prices) {
+        int[] answer = new int [prices.length];
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0; i < prices.length; i++) {
+        	while(!st.isEmpty() && prices[st.peek()] > prices[i]) {
+        		answer[st.peek()] = i-st.peek();
+        		st.pop();
+        	}
+        	st.push(i);
+		}
+
+        while(!st.isEmpty()) {
+        	answer[st.peek()] = answer.length - st.peek();
+        	st.pop();
+        }
+        return answer;
+    }
 
 }
